8ed8599709f756fdc4e3c2355b1955a90421b7af
lukasrosario <lukasrosario52699@gmail.com>
2020-11-10 17:20:03 -0500

    onboarding changes

--- re3-client/src/pages/Onboarding.js
+++ re3-client/src/pages/Onboarding.js
@@ -1,11 +1,10 @@
 import React, { useContext } from 'react';
-import SignOutButton from '../components/SignOutButton';
 import { AuthContext } from '../firebase';
 
 const Onboarding = () => {
   const user = useContext(AuthContext);
   return (
-    <div className="bg-gray-200 w-screen min-h-screen flex p-32">
+    <div className="bg-gray-200 w-screen min-h-screen flex px-32 py-16">
       <div className="w-full flex flex-col justify-start items-start">
         <div className="w-full flex flex-col justify-start items-start mb-6">
           <p className="text-5xl font-roboto text-center text-black mb-4">{`Hi ${
@@ -14,6 +13,11 @@ const Onboarding = () => {
           <p className="text-2xl font-roboto text-center text-black mb-4">
             We need some information before you can continue.
           </p>
+          <div className="w-full bg-white flex flex-col rounded-md shadow-md justify-start items-center">
+            <p className="text-3xl font-roboto font-thin text-center text-black">
+              How many years of programming experience do you have?
+            </p>
+          </div>
         </div>
       </div>
     </div>
