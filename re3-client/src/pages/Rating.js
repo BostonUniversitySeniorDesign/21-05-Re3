ac7c4263605ec36bbe90c20f677774ed2a3b4899
jyotsna-penumaka <jyotsnap@bu.edu>
2020-11-14 14:10:04 -0500

    Updating Rating.js

--- re3-client/src/pages/Rating.js
+++ re3-client/src/pages/Rating.js
@@ -1,50 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import RatingNumberButton from '../components/RatingNumberButton';
 import TestDisplayFile from '../components/TestDisplayFile';
 import Header from '../components/RatingHeader';
 import HappyFace from '../assets/img/undraw_feeling_happy_jymo.svg';
 import SadFace from '../assets/img/undraw_feeling_blue_4b7q.svg';
+import { FirebaseContext } from '../firebase';
 
 const Rating = () => {
-  const [currentAnswer, setCurrentAnswer] = useState('');
+  const firebase = useContext(FirebaseContext);
+  const [fileContents, setFileContents] = useState('');
+
+  useEffect(() => {
+    firebase.DisplayContents().then((res) => {
+      setFileContents(res);
+    });
+  }, [])
+
+  const nextSnippet = () => {
+    firebase.DisplayContents().then((res) => {
+      setFileContents(res);
+    });
+  }
+
+  const submit = (value) => {
+    firebase.addSnippetRating(parseInt(value));
+    nextSnippet();
+  };
   return (
     <div className="w-full min-h-screen bg-gray-200 flex flex-col items-center justify-start">
       <Header />
       <div className="self-start text-4xl text-black flex text-left font-hairline font-roboto py-6 px-10">
         How would you rate the readability of this code?
       </div>
-      <TestDisplayFile />
+      <TestDisplayFile snippet={fileContents} />
       <div className="flex flex-row items-center justify-center">
         <img alt="SadFace" src={SadFace} className="w-1/6 p-4" />
 
-        <RatingNumberButton answer={currentAnswer} setAnswer={setCurrentAnswer}>
+        <RatingNumberButton
+          submit={submit} 
+        >
           1
         </RatingNumberButton>
-        <RatingNumberButton answer={currentAnswer} setAnswer={setCurrentAnswer}>
+        <RatingNumberButton
+          submit={submit} 
+        >
           2
         </RatingNumberButton>
-        <RatingNumberButton answer={currentAnswer} setAnswer={setCurrentAnswer}>
+        <RatingNumberButton
+          submit={submit} 
+        >
           3
         </RatingNumberButton>
-        <RatingNumberButton answer={currentAnswer} setAnswer={setCurrentAnswer}>
+        <RatingNumberButton
+          submit={submit} 
+        >
           4
         </RatingNumberButton>
-        <RatingNumberButton answer={currentAnswer} setAnswer={setCurrentAnswer}>
+        <RatingNumberButton
+          submit={submit} 
+        >
           5
         </RatingNumberButton>
-        <RatingNumberButton answer={currentAnswer} setAnswer={setCurrentAnswer}>
+        <RatingNumberButton
+          submit={submit} 
+        >
           6
         </RatingNumberButton>
-        <RatingNumberButton answer={currentAnswer} setAnswer={setCurrentAnswer}>
+        <RatingNumberButton
+          submit={submit} 
+        >
           7
         </RatingNumberButton>
-        <RatingNumberButton answer={currentAnswer} setAnswer={setCurrentAnswer}>
+        <RatingNumberButton
+          submit={submit} 
+        >
           8
         </RatingNumberButton>
-        <RatingNumberButton answer={currentAnswer} setAnswer={setCurrentAnswer}>
+        <RatingNumberButton
+          submit={submit} 
+        >
           9
         </RatingNumberButton>
-        <RatingNumberButton answer={currentAnswer} setAnswer={setCurrentAnswer}>
+        <RatingNumberButton
+          submit={submit} 
+        >
           10
         </RatingNumberButton>
         <img alt="HappyFace" src={HappyFace} className="w-1/6 p-4" />
