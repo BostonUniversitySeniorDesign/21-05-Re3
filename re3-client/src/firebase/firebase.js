d2f24b45b6c34f093c60d48cd6362c38b7bdfda5
jyotsna-penumaka <jyotsnap@bu.edu>
2020-11-14 14:13:33 -0500

    check if user has completed onboarding question

--- re3-client/src/firebase/firebase.js
+++ re3-client/src/firebase/firebase.js
@@ -1,57 +1,3 @@
-// import app from 'firebase/app';
-// import 'firebase/auth';
-
-// const config = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID
-// };
-
-// export default class Firebase {
-//   constructor() {
-//     app.initializeApp(config);
-//     this.auth = app.auth;
-//   }
-
-//   isAuthenticated = async () => {
-//     const user = await this.auth().currentUser;
-//     return user == null ? false : true;
-//   };
-
-//   googleSignIn = async () => {
-//     try {
-//       const provider = new this.auth.GoogleAuthProvider();
-//       const result = await this.auth().signInWithPopup(provider);
-//       return result;
-//     } catch {
-//       console.log('auth error');
-//     }
-//   };
-
-//   isOnboarded = async () => {
-//     // Return if user has completed onboarding question. Can maybe be included in googleSignIn?
-//     return;
-//   };
-
-//   submitOnboarding = async () => {
-//     // Upload answer to onboarding question (and other info from google user) to db
-//     return;
-//   };
-
-//   signOut = async () => {
-//     this.auth().signOut();
-//   };
-
-//   authSubscriber = (callback) => {
-//     return this.auth().onAuthStateChanged(callback);
-//   };
-// }
-
-
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
@@ -93,7 +39,7 @@ export default class Firebase {
 
   isOnboarded = async () => {
     // Return if user has completed onboarding question. Can maybe be included in googleSignIn?
-    return;
+    return this.userOnboarded;
   };
 
   // Upload name, email, experience to firestore 
