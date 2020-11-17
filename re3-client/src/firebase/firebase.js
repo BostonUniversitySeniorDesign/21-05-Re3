8455fd3d83e184ccd361c3a5f44d1175e1d7afbe
lukasrosario <lukasrosario52699@gmail.com>
2020-11-17 07:19:58 -0500

    routing fixes

--- re3-client/src/firebase/firebase.js
+++ re3-client/src/firebase/firebase.js
@@ -12,6 +12,7 @@ const config = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
+
 export default class Firebase {
   constructor() {
     app.initializeApp(config);
@@ -20,48 +21,52 @@ export default class Firebase {
     this.storage = app.storage();
     this.currentSnippet = 1;
     this.folderName = 'gs://re3-fb.appspot.com/snippets';
-    this.userOnboarded = false;
   }
 
   isAuthenticated = async () => {
-    const user = await this.auth().currentUser;
+    const user = this.auth().currentUser;
     return user == null ? false : true;
+  };
+
+  isOnboarded = async () => {
+    const user = this.auth().currentUser;
+    if (user == null) {
+      return;
+    }
+    const ref = this.db.collection('users').doc(user.uid);
+    const exists = (await ref.get()).exists;
+    return exists;
   };
 
   googleSignIn = async () => {
     try {
       const provider = new this.auth.GoogleAuthProvider();
-      const result = await this.auth().signInWithPopup(provider);
-      return result;
-    } catch {
-      console.log('auth error');
+      await this.auth().signInWithPopup(provider);
+      return;
+    } catch (err) {
+      console.log('auth error', err);
     }
   };
 
-  isOnboarded = async () => {
-    // Return if user has completed onboarding question. Can maybe be included in googleSignIn?
-    return this.userOnboarded;
-  };
-
-  // Upload name, email, experience to firestore 
-  submitOnboarding = async (currentAnswer) => {
-    var snippet = 1;
-    console.log(currentAnswer);
+  // Upload name, email, experience to firestore
+  submitOnboarding = async (answer) => {
     const user = this.auth().currentUser;
-    this.db.collection("users").doc(user.uid).set({
-      name: user.displayName,
-      experience: currentAnswer,
-      email: user.email,
-      currentSnippet: snippet
-    }, { merge: true })
-    .then(function() {
-      this.userOnboarded = true;
-      console.log("Updated ", user.displayName, "'s info on firestore");
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
-    return;
+    const ref = this.db.collection('users').doc(user.uid);
+    const res = await ref
+      .set({
+        name: user.displayName,
+        email: user.email,
+        experience: answer,
+        isOnboarded: true,
+        currentSnippet: 1
+      })
+      .then(() => {
+        return 1;
+      })
+      .catch((error) => {
+        return -1;
+      });
+    return res;
   };
 
   signOut = async () => {
@@ -73,74 +78,105 @@ export default class Firebase {
   };
 
   downloadFile = async () => {
-    var gsRef = this.storage.refFromURL(this.folderName + "/snippet" + this.currentSnippet + '.R');
-    
-    gsRef.getDownloadURL().then(function(url) {
-      console.log(url);
-      window.open(url,"_self");
-    }).catch(function(error) {
-      console.log("error occured")
-    });
-  }  
-  
+    var gsRef = this.storage.refFromURL(
+      this.folderName + '/snippet' + this.currentSnippet + '.R'
+    );
+
+    gsRef
+      .getDownloadURL()
+      .then(function (url) {
+        console.log(url);
+        window.open(url, '_self');
+      })
+      .catch(function (error) {
+        console.log('error occured');
+      });
+  };
+
   // Display the content inside the file after fetching it from the Firebase storage
   DisplayContents = async () => {
     // Get the snippet that the current user last worked on
     const user = this.auth().currentUser;
-    var docRef = this.db.collection("users").doc(user.uid);
+    var docRef = this.db.collection('users').doc(user.uid);
     var snippet;
-    await docRef.get().then(function(doc) {
-      if (doc.exists) {
+    await docRef
+      .get()
+      .then(function (doc) {
+        if (doc.exists) {
           snippet = doc.data().currentSnippet;
-      } else {
-        console.log("No such document!");
+        } else {
+          console.log('No such document!');
         }
-      }).catch(function(error) {
-      console.log("Error getting document:", error);
-    });
+      })
+      .catch(function (error) {
+        console.log('Error getting document:', error);
+      });
     // Get the snippet from storage to display and send it the display file function
-    var gsRef = this.storage.refFromURL(this.folderName+'/snippet'+String(snippet)+'.R');
-    var url = await gsRef.getDownloadURL().then(function(url) {
-      return url;
-    }).catch(function(error) {
-      console.error("Error adding document: ", error);
+    var gsRef = this.storage.refFromURL(
+      this.folderName + '/snippet' + String(snippet) + '.R'
+    );
+    var url = await gsRef
+      .getDownloadURL()
+      .then(function (url) {
+        return url;
+      })
+      .catch(function (error) {
+        console.error('Error adding document: ', error);
+      });
+    return await fetch(url).then((res) => {
+      return res.text();
     });
-    return await fetch(url)
-      .then((res) => {return res.text()});
-  }
+  };
 
   addSnippetRating = async (rating) => {
-    if(isNaN(rating) || this.currentSnippet >= 4)
-    {
+    if (isNaN(rating) || this.currentSnippet >= 4) {
       return;
     }
     // Store rating of snippet
     var currentsnippet = this.currentSnippet;
-    var snippetString = "snippet"+currentsnippet.toString();
+    var snippetString = 'snippet' + currentsnippet.toString();
     const user = await this.auth().currentUser;
-    this.db.collection("ratings").doc(snippetString).set({
-      [user.uid]: rating
-    },{merge: true})
-    .then(function() {
-      console.log("Rating:",rating, "added to", snippetString, "from", user.uid);
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-      return;
-    });
+    this.db
+      .collection('ratings')
+      .doc(snippetString)
+      .set(
+        {
+          [user.uid]: rating
+        },
+        { merge: true }
+      )
+      .then(function () {
+        console.log(
+          'Rating:',
+          rating,
+          'added to',
+          snippetString,
+          'from',
+          user.uid
+        );
+      })
+      .catch(function (error) {
+        console.error('Error adding document: ', error);
+        return;
+      });
     // increment current snippet
-    currentsnippet = currentsnippet+1;
-    this.db.collection("users").doc(user.uid).set({
-      currentSnippet: currentsnippet
-    },{merge: true})
-    .then(function() {
-      console.log("User currentSnippet updated to", currentsnippet);
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-      return;
-    });
+    currentsnippet = currentsnippet + 1;
+    this.db
+      .collection('users')
+      .doc(user.uid)
+      .set(
+        {
+          currentSnippet: currentsnippet
+        },
+        { merge: true }
+      )
+      .then(function () {
+        console.log('User currentSnippet updated to', currentsnippet);
+      })
+      .catch(function (error) {
+        console.error('Error adding document: ', error);
+        return;
+      });
     this.currentSnippet = currentsnippet;
-  }
-
+  };
 }
