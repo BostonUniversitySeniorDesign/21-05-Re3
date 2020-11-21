b78a12f3afeb0ddbd1a7d0bbe3f80be042e0268d
lukasrosario <lukasrosario52699@gmail.com>
2020-11-21 18:41:46 -0500

    firebase config

--- re3-client/src/firebase/firebase.js
+++ re3-client/src/firebase/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: 'AIzaSyDm7t3kFLtOriXhZyOOJReon1qnuubUbvE',
+  authDomain: 're3-fb.firebaseapp.com',
+  databaseURL: 'https://re3-fb.firebaseio.com',
+  projectId: 're3-fb',
+  storageBucket: 're3-fb.appspot.com',
+  messagingSenderId: '121193880841',
+  appId: '1:121193880841:web:7d662547f9bbf78df487d8'
 };
 
 export default class Firebase {
@@ -157,7 +157,7 @@ export default class Firebase {
         return;
       });
     if (isNaN(rating) || this.currentSnippet >= 100) {
-        return;
+      return;
     }
     // increment current snippet
     currentsnippet = currentsnippet + 1;
