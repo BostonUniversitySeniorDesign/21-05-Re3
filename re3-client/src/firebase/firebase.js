adb34b8e9166cbb2c5cdcd3a764a922ead814ac1
andoliv1 <andoliv@bu.edu>
2020-12-07 09:37:00 -0300

    added api specs on firebase.js

--- re3-client/src/firebase/firebase.js
+++ re3-client/src/firebase/firebase.js
@@ -3,24 +3,24 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
-};
 export default class Firebase {
   constructor() {
-    app.initializeApp(config);
+    app.initializeApp({
+      apiKey: 'AIzaSyDm7t3kFLtOriXhZyOOJReon1qnuubUbvE',
+      authDomain: 're3-fb.firebaseapp.com',
+      databaseURL: 'https://re3-fb.firebaseio.com',
+      projectId: 're3-fb',
+      storageBucket: 're3-fb.appspot.com',
+      messagingSenderId: '121193880841',
+      appId: '1:121193880841:web:7d662547f9bbf78df487d8'
+    });
     this.auth = app.auth;
     this.db = app.firestore();
     this.storage = app.storage();
-    this.currentSnippet = null;
+    this.currentSnippet = 1;
     this.folderName = 'gs://re3-fb.appspot.com/snippets';
   }
+
 
   isAuthenticated = async () => {
     const user = this.auth().currentUser;
