2f6f07645e5e6c4c5dc74d59997555f94f7c1f45
lukasrosario <lukasrosario52699@gmail.com>
2020-12-17 19:46:08 -0500

    remove test

--- re3-client/src/pages/Rating.js
+++ re3-client/src/pages/Rating.js
@@ -65,7 +65,6 @@ const Rating = () => {
       <div className="self-center text-4xl text-black flex text-left font-hairline font-roboto py-6 px-10">
         How would you rate the readability of this code?
       </div>
-      <button onClick={() => closing()}>ok</button>
       <ProgressBar completed={completed} />
       <div className="w-full flex flex-row items-center justify-center">
         <div className="w-1/8 flex flex-col items-end justify-center pr-4">
