3e47342e035ce734b7246009b4637d314b559a45
jyotsna-penumaka <jyotsnap@bu.edu>
2021-01-10 12:56:43 -0500

    Update Onboarding.js

--- re3-client/src/pages/Onboarding.js
+++ re3-client/src/pages/Onboarding.js
@@ -47,7 +47,7 @@ const Onboarding = () => {
         setQuestionIdx(questionIdx + 1);
         break;
       default:
-        await firebase.submitOnboarding({...currentUserInfo, courseLevel: currentAnswer});
+        await firebase.submitOnboarding({...currentUserInfo, familiarWithR: currentAnswer});
         router.push('/thanks');
         break;
     }
