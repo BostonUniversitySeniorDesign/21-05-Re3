ea6e077596f7ba92ed0890893cd3b5d10f5528fc
lukasrosario <lukasrosario52699@gmail.com>
2021-02-17 02:01:02 -0500

    fixes

--- re3-client/src/components/DragAndDrop.js
+++ re3-client/src/components/DragAndDrop.js
@@ -1,28 +1,28 @@
-import React, { useState } from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import React, { useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
-const a= "Unordered";
-const b ="Ordered";
+const a = 'Unordered';
+const b = 'Ordered';
 const ColumnsFromBackend = {
-   [a]:{
-    name: "Unordered",
+  [a]: {
+    name: 'Unordered',
     items: []
   },
-   [b]:{
-    name: "Ordered",
+  [b]: {
+    name: 'Ordered',
     items: []
   }
 };
 
-const insertItems =(list, Columns)=>{
-    let newColumns= Columns.Unordered;
-    console.log("insertItem");
-    console.log(list);
-    
-    newColumns.items= list;
-}
+const insertItems = (list, Columns) => {
+  let newColumns = Columns.Unordered;
+  console.log('insertItem');
+  console.log(list);
 
-const onDragEnd = (result, columns, setColumns) => {
+  newColumns.items = list;
+};
+
+const onDragEnd = (result, columns, setColumns, setParentOrder) => {
   if (!result.destination) return;
   const { source, destination } = result;
 
@@ -34,6 +34,18 @@ const onDragEnd = (result, columns, setColumns) => {
     const [removed] = sourceItems.splice(source.index, 1);
     destItems.splice(destination.index, 0, removed);
     setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems
+      }
+    });
+    // added below
+    setParentOrder({
       ...columns,
       [source.droppableId]: {
         ...sourceColumn,
@@ -57,34 +69,52 @@ const onDragEnd = (result, columns, setColumns) => {
         items: copiedItems
       }
     });
+    // added below
+    setParentOrder({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems
+      }
+    });
     // console.log(column);
   }
-  
 };
 
-function Temp2(list) {
-insertItems(list.list, ColumnsFromBackend);
+// change below to also take a setState (setParentOrder) from parent that copies over the state of the columns in this component
+// whenever it is updated -Lukas
+function Temp2({ list, setParentOrder }) {
+  insertItems(list, ColumnsFromBackend);
   const [columns, setColumns] = useState(ColumnsFromBackend);
 
+  console.log(columns);
+
   return (
-    <div className= "flex flex-row  bg-blue-100">
+    <div className="flex flex-row  bg-blue-100">
       <DragDropContext
-        onDragEnd={result => onDragEnd(result, columns, setColumns)}
+        onDragEnd={(result) =>
+          onDragEnd(result, columns, setColumns, setParentOrder)
+        }
       >
         {Object.entries(columns).map(([columnId, column], index) => {
           return (
-            <div className="flex flex-col bg-blue-500 m-2 items-center text-2xl font-bold min-h-80 w-72 content-center rounded-md"
+            <div
+              className="flex flex-col bg-blue-500 m-2 items-center text-2xl font-bold min-h-80 w-72 content-center rounded-md"
               key={columnId}
             >
               <h2 className="m-6">{column.name}</h2>
-              <div className ="m-4 min-h-64 w-64">
+              <div className="m-4 min-h-64 w-64">
                 <Droppable droppableId={columnId} key={columnId}>
                   {(provided, snapshot) => {
                     return (
                       <div
                         {...provided.droppableProps}
                         ref={provided.innerRef}
-                        className= {`p-4 w-64 min-h-64 items-center rounded-md ${snapshot.isDraggingOver? "bg-blue-300": "bg-blue-400"}`}
+                        className={`p-4 w-64 min-h-64 items-center rounded-md ${
+                          snapshot.isDraggingOver
+                            ? 'bg-blue-300'
+                            : 'bg-blue-400'
+                        }`}
                       >
                         {column.items.map((item, index) => {
                           return (
@@ -99,11 +129,13 @@ insertItems(list.list, ColumnsFromBackend);
                                     ref={provided.innerRef}
                                     {...provided.draggableProps}
                                     {...provided.dragHandleProps}
-                                    className={`p-2 m-2 h-8 truncate text-base rounded-md ${snapshot.isDragging
-                                    ? "bg-blue-100"
-                                    : "bg-white"}`}
+                                    className={`p-2 m-2 h-8 truncate text-base rounded-md ${
+                                      snapshot.isDragging
+                                        ? 'bg-blue-100'
+                                        : 'bg-white'
+                                    }`}
                                     style={{
-                                      userSelect: "none",
+                                      userSelect: 'none',
                                       ...provided.draggableProps.style
                                     }}
                                   >
--- re3-client/src/components/DropDown.js
+++ re3-client/src/components/DropDown.js
@@ -1,23 +1,20 @@
 import React, { useState } from 'react';
 
-
-function DropDown(props) {
-
-  const [data] = useState(props.data);
-  
-  const [selectedData, updateSelectedData] = useState('');
+function DropDown({ title, data, setVersion }) {
+  const [selected, setSelected] = useState('');
 
   function handleChange(event) {
-    updateSelectedData(event.target.value);
-    props.SetRversion(selectedData);
-    // if (props.ver) props.ver.value =  selectedData;
+    setSelected(event.target.value);
+    // below changed from setVersion(version), version isn't finished updating from the line above hence the old value
+    setVersion(event.target.value);
+    // if (props.ver) props.ver.value =  selected;
   }
   let options = data.map((data) => (
     <option key={data.name} value={data.name} id={data.name}>
       {data.name}
     </option>
   ));
-//    var ver= document.getElementById('customSearch');
+  //    var ver= document.getElementById('customSearch');
   return (
     <div>
       <select
@@ -25,18 +22,16 @@ function DropDown(props) {
         id="customSearch"
         className="w-36 h-10 text-center px-4 custom-search-select border border-black rounded-md bg-gray-300 "
         onChange={handleChange}
-        value={selectedData}
+        value={selected}
       >
-        <option>
-        {props.title}
-        </option>
+        <option>{title}</option>
         {options}
       </select>
 
       {/* <button className="bg-black text-white"
-      onClick= {()=> alert(selectedData)}
+      onClick= {()=> alert(selected)}
       > show version</button>
-      {/* version of R is {selectedData} */}
+      {/* version of R is {selected} */}
     </div>
   );
 }
