2abc0dde92935fdf81c9387805717b1555dddeab
lukasrosario <lukasrosario52699@gmail.com>
2021-03-05 18:39:30 -0500

    remote builds done

--- docker-app/app.py
+++ docker-app/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request
 from flask_cors import CORS
-from subprocess import PIPE, Popen
+from subprocess import PIPE, Popen, STDOUT
 from flask_socketio import SocketIO, emit, disconnect
 from google.cloud.devtools import cloudbuild_v1
 import time
@@ -19,23 +19,12 @@ def build_and_run(version, project_ref):
     print('running')
     print(f'R_VER={version} project_ref={project_ref}')
     ref_base = os.path.basename(project_ref)
-    p3 = Popen(['gcloud', 'builds', 'submit', '--config=cloudbuild.yaml', f'--substitutions=_R_VERSION="{version}",_PROJECT_REF="{ref_base}"', '.'],
-        shell=False, stdout=PIPE, stderr=PIPE) 
-    res = ""
+
     try:
         emit('stdout', {'log': border_msg("BUILDING IMAGE...")})
-        timeout = time.time() + 60*30   # 30 minutes from now
-        while time.time() < timeout:
-            output = p3.stdout.readline()
-            if p3.poll() is not None:
-                break
-            if output:
-                emit('stdout', {'log': output.strip().decode()})
-        if time.time() >= timeout:
-            p3.kill()
-            ret = 'time limit on docker build exceeded'
-            return ret
-        # time limit for execution is one hour 
+        p3 = Popen(['gcloud', 'builds', 'submit', '--config=cloudbuild.yaml', f'--substitutions=_R_VERSION={version},_PROJECT_REF={ref_base}', '.', '--async'],
+            shell=False) 
+        res = ""
         stdout, stderr = p3.communicate()
         # if subprocess is not successful, it returns the error:
         if p3.returncode != 0:
@@ -44,32 +33,20 @@ def build_and_run(version, project_ref):
             res_str = res.decode("utf-8") if type(res) is bytes else res
             import re
             ret = re.findall(r'(?:Error).*', res_str)
+            print('problem:', ret)
             return ret
         else:
             # else returns success
             emit('stdout', {'log': border_msg("SUCCESSFULLY BUILT DOCKER IMAGE")})
     except Exception as e:
         print(e)
-        ret = "error: " + e
-        return ret
-
-    p3 = Popen(['gcloud', 'run', 'deploy', 're3', '--image', f'us-east1-docker.pkg.dev/re3-docker/re3-docker-repo/re3-image:{ref_base}', '--max-instances', '1', '--cpu', '1'],
-        shell=False, stdout=PIPE, stderr=PIPE)
-    res = ""
+        return e
+    
     try:
         emit('stdout', {'log': border_msg("ATTEMPTING TO RUN CODE...")})
-        timeout = time.time() + 60*30   # 30 minutes from now
-        while time.time() < timeout:
-            output = p3.stdout.readline()
-            if p3.poll() is not None:
-                break
-            if output:
-                emit('stdout', {'log': output.strip().decode()})
-        if time.time() >= timeout:
-            p3.kill()
-            ret = 'time limit on docker build exceeded'
-            return ret
-        # time limit for execution is one hour 
+        p3 = Popen(['gcloud', 'run', 'deploy', 're3', '--image', f'us-east1-docker.pkg.dev/re3-docker/re3-docker-repo/re3-image:{ref_base}', '--max-instances', '1', '--cpu', '1', '--region=us-east1', '--platform=managed', '--async'],
+            shell=False, stdout=STDOUT, stderr=STDOUT)
+        res = ""
         stdout, stderr = p3.communicate()
         # if subprocess is not successful, it returns the error:
         if p3.returncode != 0:
@@ -84,8 +61,8 @@ def build_and_run(version, project_ref):
             return
     # or time limit exceeded
     except Exception as e:
-        ret = "error: " + e
-        return ret
+        # print(e)
+        return e
     return 'success'
 
 def ack(version, project_ref):
