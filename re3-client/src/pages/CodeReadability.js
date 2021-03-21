c6e6d4a7a618cd00b48355e2777976f93346e74c
Layan Bahaidarah <layanb@bu.edu>
2021-03-21 15:36:40 -0400

    readability box placement fixed

--- re3-client/src/pages/CodeReadability.js
+++ re3-client/src/pages/CodeReadability.js
@@ -40,8 +40,13 @@ const CodeReadability = () => {
       .then(response => response.json()
       .then(function setThingys(data){
       console.log("hello");
+      
       setFileRating(data["readabilityScore"]);
-      setSuggestion(data["suggestion"])}
+      setSuggestion(data["suggestion"]);
+      console.log(data);
+    console.log(suggestion);
+  }
+      
       ))
       .catch(error => {
         console.error('Error: ', error);
@@ -52,44 +57,69 @@ const CodeReadability = () => {
     
 
     return (
-      <div className = "w-full relative min-h-screen bg-gray-200 flex flex-col justify-start">
-        <Header/>
-        <div className = "grid grid-cols-3 gap-y-4 justify-items-center">
-          <div className = "flex flex-col col-span-2 w-5/6">
-            <div className = "py-8">
-              <DisplayFileSmaller className = "px-2 py-4" snippet={fileContents} hidden={fileContents.length===0}/>
+      <div className="w-full relative min-h-screen bg-gray-200 flex flex-col justify-start">
+        <Header />
+        <div className="grid grid-cols-3 gap-y-4 justify-items-center">
+          <div className="flex flex-col col-span-2 w-5/6">
+            <div className="py-8">
+              <DisplayFileSmaller
+                className="px-2 py-4"
+                snippet={fileContents}
+                hidden={fileContents.length === 0}
+              />
             </div>
             {/* <form> */}
-              <label className ="text-white cursor-pointer rounded-md bg-blue-700 hover:bg-blue-500 px-10 py-2 focus:outline-none my-2 w-2/5 self-center text-center">
+            <label className="text-white cursor-pointer rounded-md bg-blue-700 hover:bg-blue-500 px-10 py-2 focus:outline-none my-2 w-2/5 self-center text-center">
               <input
                 type="file"
                 value={selectedFile}
                 onChange={(e) => update(e)}
-                className ="hidden"
+                className="hidden"
               />
               Upload File
-              </label>
+            </label>
             {/* </form> */}
           </div>
-          <div className = "flex justify-start py-8 grid grid-rows-5">
-            <div className = "row-start-1">
-              <div className="flex-none">
-                <button onClick={() => callAPI()}  className={`text-white rounded-md px-20 py-2 h-10 w-30 ${fileContents === '' ? 'bg-gray-500' : 'bg-blue-700 hover:bg-blue-500 focus:outline-none'}`} disabled = {fileContents === ''}>
-                  Get Code Rating
-                </button>    
-              </div>
-              <div className="flex-none text-center my-4">
-                <text className={`font-roboto text-xl font-bold ${parseFloat(fileRating) >= 3.0 ? (parseFloat(fileRating) >= 6.0 ? 'text-green-600' : 'text-yellow-500'): (parseFloat(fileRating) === 0.0 ? 'text-black' : 'text-red-600')}`}> {parseFloat(fileRating).toFixed(2)} </text>
-              </div>
-              <ColorBar className="flex-none" completed={(parseFloat(fileRating).toFixed(2))*10} />
+          <div className="flex flex-col justify-start py-8">
+            <div className="flex-none">
+              <button
+                onClick={() => callAPI()}
+                className={`text-white rounded-md px-20 py-2 h-10 w-30 ${
+                  fileContents === ''
+                    ? 'bg-gray-500'
+                    : 'bg-blue-700 hover:bg-blue-500 focus:outline-none'
+                }`}
+                disabled={fileContents === ''}
+              >
+                Get Code Rating
+              </button>
             </div>
-            <div className="flex-none border-blue-400 border-4 rounded-lg row-start-4 text-red-600 text-xs">
-              <text className="flex-none"> {suggestion} </text> 
+            <div className="flex-none text-center my-4">
+              <text
+                className={`font-roboto text-xl font-bold ${
+                  parseFloat(fileRating) >= 3.0
+                    ? parseFloat(fileRating) >= 6.0
+                      ? 'text-green-600'
+                      : 'text-yellow-500'
+                    : parseFloat(fileRating) === 0.0
+                    ? 'text-black'
+                    : 'text-red-600'
+                }`}
+              >
+                {' '}
+                {parseFloat(fileRating).toFixed(2)}{' '}
+              </text>
+            </div>
+            <ColorBar
+              className="flex-none"
+              completed={parseFloat(fileRating).toFixed(2) * 10}
+            />
+
+            <div className="flex flex-none resize-y px-2 py-2 w-64 h-72 my-10 border-blue-700 border-4 rounded-lg text-red-600 text-md self-center bg-white">
+              {suggestion}
             </div>
           </div>
-          
         </div>
-        
       </div>
     );
 }
